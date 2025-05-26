5aa916dba6c1e1428a9cc98972cfa05a6b147f56
jchampion136 <85515754+jchampion136@users.noreply.github.com>
2025-05-26 15:41:39 -0400

    Create helloworld.php

---
+++ PHP Helloworld/helloworld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <Title> PHP Hello World  </Title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <header>
+            <h1> PHP Hello World!</h1>
+        </header>
+        <nav>
+        <a href="index.html">Home</a>
+        <a href="pets.html">Pets</a>
+        <a href="art.html">Art</a> 
+        <a href="english.html">English</a>
+    </nav>
+
+    <h1 class="title"><?php echo "Hello, World!"; ?></h1>
+    </body>
+</html>
